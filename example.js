af992566f0cdeb99ecea8a6ab196548f03a07801
chandu-programmer <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-04-07 20:28:41 +0530

    added some stuff

--- example.js
+++ example.js
@@ -40,3 +40,33 @@ function al() {
         }
     }
 };
+
+function mode() {
+    var toOpen = document.getElementById('about');
+    var accountButton = document.getElementById('closeLine');
+    var accountButton2 = document.getElementById('closeLine2');
+    var topBar = document.getElementById('top-bar');
+    var openButton = document.getElementById('openHr')
+    var openButton2 = document.getElementById('openHr2')
+    var switchMode = document.getElementById('LDmode')
+
+    //switching background colors and colors
+    if (toOpen.style.backgroundColor === 'rgba(0, 0, 0, 0.8)') {
+        toOpen.style.backgroundColor = 'rgba(160, 160, 160, 0.8)';
+        topBar.style.backgroundColor = 'rgba(160, 160, 160, 0.8)';
+        openButton.style.backgroundColor = 'black';
+        openButton2.style.backgroundColor = 'black';
+        accountButton.style.backgroundColor = 'black';
+        accountButton2.style.backgroundColor = 'black';
+        switchMode.src = "darkmode.png"
+    }
+    else {
+        toOpen.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+        topBar.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+        openButton.style.backgroundColor = 'white';
+        openButton2.style.backgroundColor = 'white';
+        accountButton.style.backgroundColor = 'white';
+        accountButton2.style.backgroundColor = 'white';
+        switchMode.src = "mode.png"
+    }
+}
